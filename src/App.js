199e9098ec0fe08725006b32825bdb3eb58144b2
4004h <minjihwan9500@gmail.com>
2024-07-16 15:38:57 +0900

    st-3 컴포너트

--- src/App.js
+++ src/App.js
@@ -1,45 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
-function Ul() {
-  return(
-    <ul>
-    <li>개</li>
-    <li>토끼</li> 
-    <li>고양이</li>
-    <li>거북이</li>
-  </ul>
-  )
-}
-function Table() {
-  return(
-    <table border="1px">
-      <tr>
-        <th>이름</th>
-        <th>나이</th>
-      </tr>
-      <tr>
-        <td>홍길동</td>
-        <td>24</td>
-      </tr>
-      <tr>
-        <td>이순신</td>
-        <td>44</td>
-      </tr>
-    </table>
-  )
-}
-
 function App() {
   return (
-    <div className="App">
-      <Ul></Ul>
-      <Table></Table>
-      {/* <Ul/> <Table/> */}
+    <>
+      <p>안녕하세여</p>
+      <p>반가워여</p>
 
-
-    </div>
-  );
+    </>    
+  )
 }
 
 export default App;
