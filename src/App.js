32a31f4c7e7754d485cea071849228f190aedbeb
4004h <minjihwan9500@gmail.com>
2024-07-16 15:14:33 +0900

    st-2.1

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Ul() {
+  return(
+    <ul>
+    <li>개</li>
+    <li>토끼</li> 
+    <li>고양이</li>
+    <li>거북이</li>
+  </ul>
+  )
+}
+function Table() {
+  return(
+    <table border="1px">
+      <tr>
+        <th>이름</th>
+        <th>나이</th>
+      </tr>
+      <tr>
+        <td>홍길동</td>
+        <td>24</td>
+      </tr>
+      <tr>
+        <td>이순신</td>
+        <td>44</td>
+      </tr>
+    </table>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <ul>
-        <li>개</li>
-        <li>토끼</li> 
-        <li>고양이</li>
-        <li>거북이</li>
-      </ul>
-      <table border="1px">
-        <tr>
-          <th>이름</th>
-          <th>나이</th>
-        </tr>
-        <tr>
-          <td>홍길동</td>
-          <td>24</td>
-        </tr>
-        <tr>
-          <td>이순신</td>
-          <td>44</td>
-        </tr>
-      </table>
-
+      <Ul></Ul>
+      <Table></Table>
+      {/* <Ul/> <Table/> */}
 
 
     </div>
