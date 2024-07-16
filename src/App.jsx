e87de4ea1f2ee7dbcac21758c17e296f5d214eb0
4004h <minjihwan9500@gmail.com>
2024-07-16 17:02:05 +0900

    st-3.2 컴포너 props

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
-
+import ColoredMessage from "./components/ColoredMessage.jsx"
+import ColoredMessage2 from "./components/ColoredMessage2.jsx"
 
 function App() { //함수명
-  const style = {border:"1px solid black"}
   const aaa = (param)=>{
     let dog = param.target.getAttribute( 'data' )
     alert( dog )
   }
-
+  const redStyle = {
+    color:"red",
+    backgroundColor:"yellow"
+  }
+  const pinkStyle = {
+    color : "pink",
+    fontSize:"20px"
+  }
   return (
-    <div style = {style}>
+    <div style = {{border:"1px solid black"}}>
 
-      <p>안녕하세여</p>
-      <p>반가워여</p>
+      <p style={{color:"blue",fontSize:"32px"}}>안녕하세여</p>
+      {/* <p style={redStyle}>반가워여 잘지내지</p> */}
+      <ColoredMessage color="orange" message="잘지내시죠?"/>
+      <ColoredMessage color="red" message="how do you do"/>
+      <ColoredMessage2 color="blue">잘지내</ColoredMessage2>
+      <p style={ pinkStyle }>잘지내</p>
       <button onClick={aaa} data="강아지">버튼</button>
 
     </div>
